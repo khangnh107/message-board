665d108e637c2f177ce03c2d62938d4e1ded816f
Khang Huu Nguyen <nhk2005.personal@gmail.com>
2023-12-08 09:48:52 -0500

    Combine all routers into one file

--- routes/form.js
+++ routes/form.js
@@ -5,4 +5,13 @@ router.get("/", (req, res, next) => {
   res.render("form");
 });
 
+router.post("/", (req, res, next) => {
+  messages.push({
+    text: req.message,
+    user: req.user,
+    dateAdded: new Date(),
+  });
+  res.redirect("/");
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,21 @@ const messages = [
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Message Board", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("new");
+});
+
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.message,
+    user: req.user,
+    dateAdded: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
